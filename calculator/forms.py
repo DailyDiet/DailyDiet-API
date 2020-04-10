a3a76fa459b99abd61705239e34d2712a4e217a8
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-10 17:45:45 +0430

    add calorie form

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,11 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, DecimalField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms import DecimalField, IntegerField, StringField, FloatField
+from wtforms.validators import AnyOf, DataRequired, NumberRange
 
 
 class BMIForm(FlaskForm):
     class Meta:
         csrf = False
 
-    height = IntegerField('height', validators=[DataRequired(), NumberRange(50, 210)])  # height in centimeter
-    weight = DecimalField('weight', validators=[DataRequired(), NumberRange(20, 150)])  # weight in kilograms
+    height = IntegerField('Height', validators=[DataRequired(), NumberRange(50, 210)])  # height in centimeter
+    weight = DecimalField('Weight', validators=[DataRequired(), NumberRange(20, 150)])  # weight in kilograms
+
+
+class CalorieForm(FlaskForm):
+    class Meta:
+        csrf = False
+    
+    goal = StringField('Goal', validators=[DataRequired(), AnyOf(['lose_weight', 'maintain', 'build_muscle'])])
+    gender = StringField('Gender', validators=[DataRequired(), AnyOf(['male', 'female'])]) 
+    height = IntegerField('Height', validators=[DataRequired(), NumberRange(50, 210)])  # height in centimeter
+    weight = DecimalField('Weight', validators=[DataRequired(), NumberRange(20, 150)])  # weight in kilograms
+    age = IntegerField('Age', validators=[DataRequired(), NumberRange(1, 120)])         # age in years
+    activity = StringField('Activity Level', validators=[DataRequired(), AnyOf(['sedentary', 'lightly', 'moderately', 'very', 'extra'])])
