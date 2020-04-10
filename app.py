7949f751e1d4951dcf350f34f7f91c1e5cfe5f8f
nimaafshar <nima.afsharh@gmail.com>
2020-04-10 16:47:16 +0430

    some changes made to app running.

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ def create_app(environment='Development'):
     """
     :param environment: is either Development/Production/Testing
     """
+    if environment is None:
+        environment = 'Development'
     app = Flask(__name__)
     app.config.from_object(f'config.{environment}Config')
     app.register_blueprint(calculator)
@@ -15,8 +17,7 @@ def create_app(environment='Development'):
     return app
 
 
-app = create_app()
+app = create_app(environment=getenv('DAILYDIET_ENV'))
 
 if __name__ == '__main__':
     app.run()
-    # todo:make app running clean
