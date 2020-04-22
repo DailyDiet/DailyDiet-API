6ec9fe1613298e31c9169e950dfe79ce5dabde57
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-22 19:30:39 +0430

    Add SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
-
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
     DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
     DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-    MONGO_URI = f'mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@dailydiet-qeynf.mongodb.net/test?retryWrites=true&w=majority'
-
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
 class DevelopmentConfig(Config):
     DEBUG = True
