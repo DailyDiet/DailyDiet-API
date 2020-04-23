20c505069ce59f03760173109b46b6628a63da8e
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-24 03:18:22 +0430

    FullName field added

--- users/forms.py
+++ users/forms.py
@@ -1,31 +1,41 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms import PasswordField, TextField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 from users.models import User
 
 
-class LoginForm(Form):
-    email = TextField('email', validators=[DataRequired(), Email()])
-    password = PasswordField('password', validators=[DataRequired()])
-
-
 class RegisterForm(Form):
+    class Meta:
+        csrf = False
+
+    full_name = TextField('full name', validators=[DataRequired(), Length(max=70)])
     email = TextField('email', validators=[DataRequired(), Email(message=None), Length(min=6, max=40)])
     password = PasswordField( 'password', validators=[DataRequired(), Length(min=6, max=25)])
-    confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
+    confirm_password = PasswordField('repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
 
     def validate(self):
         initial_validation = super(RegisterForm, self).validate()
         if not initial_validation:
             return False
-        user = User.query.filter_by(email=self.email.data).first()
+        user = User.query.filter_by(Email=self.email.data).first()
         if user:
-            self.email.errors.append("Email already registered")
+            self.email.errors.append("Email already registered.")
             return False
         return True
 
 
+class LoginForm(Form):
+    class Meta:
+        csrf = False
+
+    email = TextField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
 class ChangePasswordForm(Form):
+    class Meta:
+        csrf = False
+
     password = PasswordField('password', validators=[DataRequired(), Length(min=6, max=25)])
-    confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
+    confirm_password = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
--- users/models.py
+++ users/models.py
@@ -11,21 +11,24 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = Column(Integer(), primary_key=True)
-    email = Column(String(), unique=True, nullable=False)
-    password = Column(String(), nullable=False)
-    registered_on = Column(DateTime(), nullable=False)
-    admin = Column(Boolean(), nullable=False, default=False)
-    confirmed = Column(Boolean(), nullable=False, default=False)
-    confirmed_on = Column(DateTime(), nullable=True)
+    FullName = Column(String(), unique=False, nullable=False)
+    Email = Column(String(), unique=True, nullable=False)
+    Password = Column(String(), nullable=False)
+    Admin = Column(Boolean(), nullable=False, default=False)
+    RegisteredOn = Column(DateTime(), nullable=False)
+    Confirmed = Column(Boolean(), nullable=False, default=False)    # Confirmed Email Address Or Not
+    ConfirmedOn = Column(DateTime(), nullable=True)
 
-    def __init__(self, email, password, confirmed,
-                 paid=False, admin=False, confirmed_on=None):
-        self.email = email
-        self.password = generate_password_hash(password)
-        self.registered_on = datetime.datetime.now()
-        self.admin = admin
-        self.confirmed = confirmed
-        self.confirmed_on = confirmed_on
+    def __init__(self, full_name, email, password, admin=False,
+                 registerd_on=None, confirmed=False, confirmed_on=None):
+
+        self.FullName = full_name
+        self.Email = email
+        self.Password = generate_password_hash(password)
+        self.Admin = admin
+        self.RegisteredOn = registerd_on or datetime.datetime.now()
+        self.Confirmed = confirmed
+        self.ConfirmedOn = confirmed_on
 
     def is_authenticated(self):
         return True
@@ -40,4 +43,4 @@ class User(db.Model):
         return self.id
 
     def __repr__(self):
-        return f'<email {self.email}>'
+        return f'<Email {self.Email}>'
