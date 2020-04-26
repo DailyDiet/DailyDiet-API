45e9be6114ab37e5a25c4ef983ae295d91e46a38
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-26 13:04:03 +0430

    Mail config added

--- config.py
+++ config.py
@@ -3,11 +3,25 @@ import os
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
-    DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
-    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
+
+    # database 
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
+    # mail settings
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_MAX_EMAILS = None
+
+    # gmail authentication
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
 
 class DevelopmentConfig(Config):
     DEBUG = True
