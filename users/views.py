814515a5cb90cdbff6114bae03d01ad5c203bac6
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-04-27 19:48:19 +0430

    resend confirmation view added

--- users/views.py
+++ users/views.py
@@ -11,11 +11,11 @@ from users.email import send_email
 from users.forms import ChangePasswordForm, LoginForm, RegisterForm
 from users.models import User
 from users.token import confirm_token, generate_confirmation_token
-from utils.decorators import json_only
+from utils.decorators import confirmed_only, json_only
 
 
 @users.route('/signup', methods=['POST'])
-def register():
+def create_user():
     form = RegisterForm()
     if not form.validate_on_submit():
         return {'errors': form.errors}, 400
@@ -23,28 +23,30 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     token = generate_confirmation_token(new_user.Email)
-    subject = "DailyDiet | Please confirm your email"
+    subject = 'DailyDiet | Email Confirmation'
     confirm_url = url_for('users.confirm_email', token=token, _external=True)
     html = html = render_template('users/activate.html', confirm_url=confirm_url)
     send_email(new_user.Email, subject, html)
     return {'msg': 'Account created successfully!'}, 201
 
 
-@users.route('/confirm/<token>', methods=['GET'])
+@users.route('/signup/confirmation/<token>', methods=['GET'])
 def confirm_email(token):
     try:
         email = confirm_token(token)
     except:
         return {'error': 'The confirmation link is invalid or has expired.'}, 400
-    user = User.query.filter_by(Email=email).first_or_404()
+    user = User.query.filter_by(Email=email).first()
+    if not user:
+        return {'error': 'User not found.'}, 404
     if user.Confirmed:
-        return {'msg': 'Account already confirmed. Please login.'}
+        return {'error': 'Account already confirmed. Please login.'}, 400
     else:
         user.Confirmed = True
         user.ConfirmedOn = datetime.datetime.now()
         db.session.add(user)
         db.session.commit()
-        return {'msg': 'You have confirmed your account. Thanks!'}
+        return {}, 204
 
 
 @users.route('/signin', methods=['POST'])
@@ -54,11 +56,11 @@ def login():
         return {'errors': form.errors}, 400
     email = form.email.data
     password = form.password.data
-    user = User.query.filter(User.Email.ilike(email)).first()
+    user = User.query.filter_by(Email=email).first()
     if not user:
-        return {'error': 'Email/Password does not match.'}, 403
+        return {'error': 'Email or Password does not match.'}, 403
     if not user.check_password(password):
-        return {'error': 'Email/Password does not match.'}, 403
+        return {'error': 'Email or Password does not match.'}, 403
     access_token = create_access_token(identity=user.Email, fresh=True)
     refresh_token = create_refresh_token(identity=user.Email)
     return {'access_token': access_token, 'refresh_token': refresh_token}, 200
@@ -71,11 +73,23 @@ def get_new_access_token():
     return {'access_token': f'{create_access_token(identity=identity)}'}
 
 
-@users.route('/signup', methods=['PATCH'])
+@users.route('/signup/resendConfrimation', methods=['GET'])
+@jwt_required
+def resend_confirmation():
+    identity = get_jwt_identity()
+    token = generate_confirmation_token(identity)
+    confirm_url = url_for('users.confirm_email', token=token, _external=True)
+    html = render_template('users/activate.html', confirm_url=confirm_url)
+    subject = 'DailyDiet | Email Confirmation'
+    send_email(identity, subject, html)
+    return {'msg': 'A new confirmation email has been sent.'}, 200
+
+
+@users.route('/signup/modify', methods=['PATCH'])
 @jwt_required
 def change_password():
     identity = get_jwt_identity()
-    user = User.query.filter(User.Email.ilike(identity)).first()
+    user = User.query.filter_by(Email=identity).first()
     form = ChangePasswordForm()
     if not user.check_password(form.old_password.data):
         return {'error': 'Old password does not match.'}, 403
