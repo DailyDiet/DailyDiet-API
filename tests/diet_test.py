1fab15e9f17bf9c485ea62cb7f6da7c79fed1a35
parsaalian0 <yassi.ommi@gmail.com>
2020-05-08 15:47:34 +0430

    oops.

--- tests/diet_test.py
+++ tests/diet_test.py
@@ -7,7 +7,7 @@ ip = getenv('TEST_URL')
 class TestDiet(unittest.TestCase):
 
     def test_sevade(self):
-        r = requests.get(ip + '/food/sevade', json={'calorie': 2300})
+        r = requests.get(ip + '/food/sevade/2300')
         result = r.result()
         assert result['diet'][3] in range(2990, 2310, 1), 'Test Failed :('
 
