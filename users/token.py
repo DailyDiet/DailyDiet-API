6e62b577a3f8cb8f6f23b45b8d2021fbf3095038
Nima <nimaafshar79@gmail.com>
2020-05-21 00:59:08 +0430

    config used as an unsubscribeable object.

--- users/token.py
+++ users/token.py
@@ -4,16 +4,16 @@ from config import Config
 
 
 def generate_confirmation_token(email):
-    serializer = URLSafeTimedSerializer(Config['SECRET_KEY'])
-    return serializer.dumps(email, salt=Config['SECURITY_PASSWORD_SALT'])
+    serializer = URLSafeTimedSerializer(Config.SECRET_KEY)
+    return serializer.dumps(email, salt=Config.SECURITY_PASSWORD_SALT)
 
 
 def confirm_token(token, expiration=3600):
-    serializer = URLSafeTimedSerializer(Config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(Config.SECRET_KEY)
     try:
         email = serializer.loads(
             token,
-            salt= Config['SECURITY_PASSWORD_SALT'],
+            salt=Config.SECURITY_PASSWORD_SALT,
             max_age=expiration
         )
     except:
