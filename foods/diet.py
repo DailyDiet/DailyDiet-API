80154579417f39c7014f6941f9f97719abca6bb0
parsaalian0 <yassi.ommi@gmail.com>
2020-05-24 22:01:47 +0430

    commit

--- foods/diet.py
+++ foods/diet.py
@@ -9,7 +9,7 @@ def sevade(list1, list2, list3, calorie):
         while j < len(list2) and k >= 0:
             if list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie() in range(n-rand, n+rand, 1):
                 return list1[i], list2[j], list3[k], list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie()
-            elif list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie() < n-10:
+            elif list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie() < calorie-10:
                 j += 1
             else:
                 k -=1
