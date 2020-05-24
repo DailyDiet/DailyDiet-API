aaf7a1ac9e4b3c4c44d7993c5c96cc180ac21245
parsaalian0 <yassi.ommi@gmail.com>
2020-05-24 22:32:19 +0430

    test

--- foods/diet.py
+++ foods/diet.py
@@ -4,14 +4,18 @@ def sevade(list1, list2, list3, calorie):
     i = 0
     j = 0
     k = len(list3)-1
-    rand = random.randint(1,10)
-    while i < len(list1):
-        while j < len(list2) and k >= 0:
-            if float(list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie()) in range(calorie-rand, calorie+rand, 1):
-                return list1[i], list2[j], list3[k], list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie()
-            elif float(list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie()) < calorie-10:
-                j += 1
-            else:
-                k -=1
-        i +=1
+    print(list1[i].get_calorie(), list2[j].get_calorie(), list3[k].get_calorie())
+    print(type(list1[i].get_calorie()))
+    print(list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie())
+    print(type(list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie()))
+    # rand = random.randint(1,10)
+    # while i < len(list1):
+    #     while j < len(list2) and k >= 0:
+    #         if list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie() in range(calorie-rand, calorie+rand, 1):
+    #             return list1[i], list2[j], list3[k], list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie()
+    #         elif list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie() < calorie-10:
+    #             j += 1
+    #         else:
+    #             k -=1
+    #     i +=1
     return None
