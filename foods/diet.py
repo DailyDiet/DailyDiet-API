f10e4988ca3860b0269b2512b389e74d427d6a16
parsaalian0 <yassi.ommi@gmail.com>
2020-05-24 23:06:53 +0430

    TD-LTE

--- foods/diet.py
+++ foods/diet.py
@@ -5,11 +5,12 @@ def sevade(list1, list2, list3, calorie):
     j = 0
     k = len(list3)-1
     rand = random.randint(1,10)
+    n = int(calorie)
     while i < len(list1):
         while j < len(list2) and k >= 0:
-            if list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie() in range(calorie-rand, calorie+rand, 1):
-                return list1[i], list2[j], list3[k], list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie()
-            elif list1[i].get_calorie() + list2[j].get_calorie() + list3[k].get_calorie() < calorie-10:
+            if float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie()) in range(n-rand, n+rand, 1):
+                return list1[i], list2[j], list3[k], float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie())
+            elif float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie()) < n-10:
                 j += 1
             else:
                 k -=1
