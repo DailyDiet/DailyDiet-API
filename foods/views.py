e9e46e35206fcc4c666b752e0c2226015d419a4c
parsaalian0 <yassi.ommi@gmail.com>
2020-05-25 17:27:31 +0430

    diets are coming chap o rast

--- foods/views.py
+++ foods/views.py
@@ -1,14 +1,14 @@
 from foods import foods
 from flask import jsonify
 from foods.utils import get_foods_with_categories
-from foods.diet import sevade, yevade
+from foods.diet import sevade, yevade, dovade
 from .models import Food
 from flask_jwt_extended import (jwt_required)
 from utils.decorators import confirmed_only
 
 @foods.route('/yevade/<int:calorie>', methods=['GET'])
 def get_yevade(calorie):
-    cat = ['breakfast, ''mostly_meat', 'pasta', 'main_dish', 'sandwich']
+    cat = ['breakfast, ''mostly_meat', 'pasta', 'main_dish', 'sandwich', 'appetizers', 'drink']
     dog = get_foods_with_categories(cat)
 
     catdog = yevade(dog, calorie)
@@ -19,11 +19,27 @@ def get_yevade(calorie):
         return jsonify({'diet': [str(catdog[0]), catdog[1]]}), 200
 
 
+@foods.route('/dovade/<int:calorie>', methods=['GET'])
+def get_dovade(calorie):
+    cats1 = ['breakfast', 'sandwich', 'pasta', 'appetizers', 'drink']
+    cats2 = ['mostly_meat', 'pasta', 'main_dish', 'sandwich', 'appetizers']
+
+    dogs1 = get_foods_with_categories(cats1)
+    dogs2 = get_foods_with_categories(cats2)
+
+    catdog = dovade(dogs1, dogs2, calorie)
+
+    if catdog is None:
+        return jsonify({'error': 'Not Found'}), 404
+    else:
+        return jsonify({'diet': [str(catdog[0]), str(catdog[1]), str(catdog[2])]}), 200
+
+
 @foods.route('/sevade/<int:calorie>', methods=['GET'])
 def get_sevade(calorie):
-    cats1 = ['breakfast']
+    cats1 = ['breakfast', 'pasta', 'salad', 'sandwich', 'appetizers']
     cats2 = ['mostly_meat', 'pasta', 'main_dish', 'sandwich']
-    cats3 = ['dessert', 'other', 'salad']
+    cats3 = ['dessert', 'other', 'salad', 'side_dish', 'drink', 'main_dish', 'pasta']
 
     dogs1 = get_foods_with_categories(cats1)
     dogs2 = get_foods_with_categories(cats2)
