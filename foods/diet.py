397559a192be93ca1127e3b2be95daa157f29c1f
parsaalian0 <yassi.ommi@gmail.com>
2020-05-25 17:30:01 +0430

    diets

--- foods/diet.py
+++ foods/diet.py
@@ -1,18 +1,39 @@
 import random
 
+def yevade(list1, calorie):
+    i = 0
+    rand = random.randint(1,10)
+    while i < len(list1):
+        if float(list1[i].get_calorie()) in range (calorie-rand, calorie+rand, 1):
+            return list1[i], float(list1[i].get_calorie())
+        i += 1
+    return None
+
+def dovade(list1, list2, calorie):
+    i = 0
+    j = len(list2) - 1
+    rand = random.randint(1,10)
+    while i < len(list1) and j>=0:
+        if float(list1[i].get_calorie()) + float(list2[j].get_calorie()) in range(calorie-rand, calorie+rand, 1):
+            return list1[i], list2[j], float(list1[i].get_calorie()) + float(list2[j].get_calorie())
+        elif float(list1[i].get_calorie()) + float(list2[j].get_calorie()) < calorie - 10:
+            j -= 1
+        i += 1
+    return None
+
+
 def sevade(list1, list2, list3, calorie):
     i = 0
     j = 0
-    k = len(list3)-1
+    k = len(list3) - 1
     rand = random.randint(1,10)
-    n = int(calorie)
     while i < len(list1):
         while j < len(list2) and k >= 0:
-            if float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie()) in range(n-rand, n+rand, 1):
+            if float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie()) in range(calorie-rand, calorie+rand, 1):
                 return list1[i], list2[j], list3[k], float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie())
-            elif float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie()) < n-10:
+            elif float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie()) < calorie-10:
                 j += 1
             else:
-                k -=1
-        i +=1
+                k -= 1
+        i += 1
     return None
--- foods/views.py
+++ foods/views.py
@@ -1,15 +1,45 @@
 from foods import foods
 from flask import jsonify
 from foods.utils import get_foods_with_categories
-from foods.diet import sevade
+from foods.diet import sevade, yevade, dovade
 from .models import Food
+from flask_jwt_extended import (jwt_required)
+from utils.decorators import confirmed_only
+
+@foods.route('/yevade/<int:calorie>', methods=['GET'])
+def get_yevade(calorie):
+    cat = ['breakfast, ''mostly_meat', 'pasta', 'main_dish', 'sandwich', 'appetizers', 'drink']
+    dog = get_foods_with_categories(cat)
+
+    catdog = yevade(dog, calorie)
+
+    if catdog is None:
+        return jsonify({'error': 'Not Found'}), 404
+    else:
+        return jsonify({'diet': [str(catdog[0]), catdog[1]]}), 200
 
 
-@foods.route('/sevade/<calorie>', methods=['GET'])
+@foods.route('/dovade/<int:calorie>', methods=['GET'])
+def get_dovade(calorie):
+    cats1 = ['breakfast', 'sandwich', 'pasta', 'appetizers', 'drink']
+    cats2 = ['mostly_meat', 'pasta', 'main_dish', 'sandwich', 'appetizers']
+
+    dogs1 = get_foods_with_categories(cats1)
+    dogs2 = get_foods_with_categories(cats2)
+
+    catdog = dovade(dogs1, dogs2, calorie)
+
+    if catdog is None:
+        return jsonify({'error': 'Not Found'}), 404
+    else:
+        return jsonify({'diet': [str(catdog[0]), str(catdog[1]), str(catdog[2])]}), 200
+
+
+@foods.route('/sevade/<int:calorie>', methods=['GET'])
 def get_sevade(calorie):
-    cats1 = ['breakfast']
+    cats1 = ['breakfast', 'pasta', 'salad', 'sandwich', 'appetizers']
     cats2 = ['mostly_meat', 'pasta', 'main_dish', 'sandwich']
-    cats3 = ['dessert', 'other', 'salad']
+    cats3 = ['dessert', 'other', 'salad', 'side_dish', 'drink', 'main_dish', 'pasta']
 
     dogs1 = get_foods_with_categories(cats1)
     dogs2 = get_foods_with_categories(cats2)
