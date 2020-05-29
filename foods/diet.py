fda90d082d55aa568c9ea849d05f3663a6a9c72a
parsaalian0 <yassi.ommi@gmail.com>
2020-05-29 13:04:48 +0430

    diet tests

--- foods/diet.py
+++ foods/diet.py
@@ -2,7 +2,7 @@ import random
 
 def yevade(list1, calorie):
     i = 0
-    rand = random.randint(1,10)
+    rand = random.randint(1,30)
     while i < len(list1):
         if float(list1[i].get_calorie()) in range (calorie-rand, calorie+rand, 1):
             return list1[i], float(list1[i].get_calorie())
@@ -12,7 +12,7 @@ def yevade(list1, calorie):
 def dovade(list1, list2, calorie):
     i = 0
     j = len(list2) - 1
-    rand = random.randint(1,10)
+    rand = random.randint(1,30)
     while i < len(list1) and j>=0:
         if float(list1[i].get_calorie()) + float(list2[j].get_calorie()) in range(calorie-rand, calorie+rand, 1):
             return list1[i], list2[j], float(list1[i].get_calorie()) + float(list2[j].get_calorie())
@@ -26,7 +26,7 @@ def sevade(list1, list2, list3, calorie):
     i = 0
     j = 0
     k = len(list3) - 1
-    rand = random.randint(1,10)
+    rand = random.randint(1,30)
     while i < len(list1):
         while j < len(list2) and k >= 0:
             if float(list1[i].get_calorie()) + float(list2[j].get_calorie()) + float(list3[k].get_calorie()) in range(calorie-rand, calorie+rand, 1):
--- tests/diet_test.py
+++ tests/diet_test.py
@@ -12,5 +12,23 @@ class TestDiet(unittest.TestCase):
         assert result['diet'][3] in range(2990, 2310, 1), 'Test Failed :('
 
     def test_zero_sevade(self):
-        r = requests.get(ip + '/food/sevade', json={'calorie': 0})
+        r = requests.get(ip + '/food/sevade/0')
+        assert r.status_code == 404, 'Khak bar saret :('
+
+    def test_dovade(self):
+        r = requests.get(ip + '/food/dovade/4300')
+        result = r.result()
+        assert result['diet'][2] in range(2990, 2310, 1), 'Test Failed :('
+
+    def test_zero_dovade(self):
+        r = requests.get(ip + '/food/dovade/0')
+        assert r.status_code == 404, 'Khak bar saret :('
+
+    def test_yevade(self):
+        r = requests.get(ip + '/food/yevade/400')
+        result = r.result()
+        assert result['diet'][1] in range(390, 410, 1), 'Test Failed :('
+
+    def test_zero_yevade(self):
+        r = requests.get(ip + '/food/yevade/0')
         assert r.status_code == 404, 'Khak bar saret :('
