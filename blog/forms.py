722a6b33a86e60021073195afb990fe5f1789ec2
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-06-04 01:38:27 +0430

    check slug and title are unique

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField
 from wtforms.validators import DataRequired
+from blog.models import Post
 
 
 class PostForm(FlaskForm):
@@ -11,13 +12,18 @@ class PostForm(FlaskForm):
     summary = TextAreaField()
     content = TextAreaField(validators=[DataRequired()])
     slug = TextField(validators=[DataRequired()])
-    categories = TextField(validators=[DataRequired()])
+    category = TextField(validators=[DataRequired()])
 
-
-class CategoryForm(FlaskForm):
-    class Meta:
-        csrf = False
-
-    name = TextField(validators=[DataRequired()])
-    slug = TextField(validators=[DataRequired()])
-    description = TextAreaField()
+    def validate(self):
+        initial_validation = super(PostForm, self).validate()
+        if not initial_validation:
+            return False
+        slug = Post.query.filter_by(slug=self.slug.data).first()
+        title = Post.query.filter_by(title=self.title.data).first()
+        if slug:
+            self.slug.errors.append("slug has already been taken.")
+            return False
+        if title:
+            self.title.errors.append("title has already been taken.")
+            return False
+        return True
