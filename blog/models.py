c7345cba518ed67188995717d60c7d51deb9a3c1
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-06-03 16:24:48 +0430

    add Blog model

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Text, Table, ForeignKey
+from app import db
+
+
+posts_categories = Table('posts_categories', db.metadata,
+    Column('post_id', Integer, ForeignKey('posts.id', ondelete='cascade')),
+    Column('categorie_id' , Integer, ForeignKey('categories.id', ondelete='cascade'))
+)
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False, unique=True) 
+    description = Column(String(256), nullable=True, unique=False)
+    slug = Column(String(128), nullable=False, unique=True)
+    posts = db.relationship('Post', secondary=posts_categories, back_populates='categories')
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), nullable=False, unique=True) 
+    summary = Column(String(256), nullable=True, unique=False)
+    content = Column(Text, nullable=False, unique=False)
+    slug = Column(String(128), nullable=False, unique=True)
+    categories = db.relationship('Category', secondary=posts_categories, back_populates='posts')
