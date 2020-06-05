c968f11b21fe4d89aab2e2db12207be174d624a9
Mohammad Hossein Malekpour <43685227+mhmp98@users.noreply.github.com>
2020-06-05 16:12:39 +0430

    edit routes

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .forms import PostForm
 from .models import Post
 
 
-@blog.route('/posts/', methods=['GET'])
+@blog.route('/', methods=['GET'])
 def list_posts():
     posts = Post.query.order_by(Post.id.desc()).all()
     result = dict()
@@ -20,14 +20,14 @@ def list_posts():
     return jsonify(result), 200
 
 
-@blog.route('/posts/<string:slug>', methods=['GET'])
+@blog.route('/<string:slug>', methods=['GET'])
 def single_post(slug):
     post = Post.query.filter(Post.slug == slug).first()
     result = {'slug': post.slug, 'title' : post.title, 'summary': post.summary, 'content': post.content, 'category': post.category}
     return jsonify(result), 200
 
 
-@blog.route('/create_post', methods=['POST'])
+@blog.route('/posts/new/', methods=['POST'])
 def create_post():
     form = PostForm()
     if not form.validate_on_submit():
@@ -43,9 +43,24 @@ def create_post():
     return {'msg': 'Post created successfully'}, 201
 
 
-@blog.route('/delete/<int:post_id>', methods=['DELETE'])
+@blog.route('/posts/delete/<int:post_id>/', methods=['DELETE'])
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
     db.session.delete(post)
     db.session.commit()
     return {}, 204
+
+
+# @blog.route('/posts/modify/<int:post_id>/', methods=['PATCH'])
+# def modify_post(post_id):
+#     post = Post.query.get_or_404(post_id)
+#     form = PostForm()
+#     if not form.validate_on_submit():
+#         return {'error': form.errors}, 400
+#         new_post.title = form.title.data
+#     post.content = form.content.data
+#     post.slug = form.slug.data
+#     post.summary = form.summary.data
+#     post.category = form.category.data
+#     db.session.commit()
+#     return {'msg': 'Post Modified!'}, 201
